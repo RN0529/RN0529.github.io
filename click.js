7ec4d10b3c37a4b035d23c211d8dfc64ba54367e
RN0529 <richardngowork@gmail.com>
2021-06-25 16:42:25 -0400

    added onlcick for projects

---
+++ click.js
@@ -0,0 +1,10 @@
+var projectsButton = document.getElementById('toProjects');
+var elmntToView;
+function projectsOnClick(){
+    elmntToView = document.getElementById("projects");
+    elmntToView.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+}
+function contactOnClick(){
+    elmntToView = document.getElementById("projects");
+    elmntToView.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+}
