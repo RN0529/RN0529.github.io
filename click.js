cf6cca38110b1dc93d2ea49e935a7e5127fc712f
RN0529 <richardngowork@gmail.com>
2021-06-25 16:57:36 -0400

    added about div and about button as well as edited other on click functiosn

--- click.js
+++ click.js
@@ -1,10 +1,14 @@
-var projectsButton = document.getElementById('toProjects');
+
 var elmntToView;
 function projectsOnClick(){
-    elmntToView = document.getElementById("projects");
+    elmntToView = document.getElementById("projects2");
     elmntToView.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
 function contactOnClick(){
-    elmntToView = document.getElementById("projects");
+    elmntToView = document.getElementById("bottom");
+    elmntToView.scrollIntoView({ block: 'start',  behavior: 'smooth' });
+}
+function aboutOnClick(){
+    elmntToView = document.getElementById("projects1");
     elmntToView.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
