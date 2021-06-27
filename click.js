f7cdcf7367cfe5019e3270c55576022e9a12eacd
RN0529 <richardngowork@gmail.com>
2021-06-26 23:53:02 -0400

    added side nav bar : ^ )

--- click.js
+++ click.js
@@ -12,3 +12,14 @@ function aboutOnClick(){
     elmntToView = document.getElementById("projects1");
     elmntToView.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("navbar").style.width = "250px";
+
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("navbar").style.width = "0";
+  }
