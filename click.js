3e97fff180839e203d1404b08e5a39f6226f7dcc
RN0529 <richardngowork@gmail.com>
2021-06-29 15:50:37 -0400

    added git and linkedin links to side bar and altered some designs

--- click.js
+++ click.js
@@ -20,10 +20,12 @@ function upbtnonclick(){
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("navbar").style.width = "250px";
+    document.getElementById("body").style.paddingLeft = "250px";
 
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
-    document.getElementById("navbar").style.width = "0";
+    document.getElementById("navbar").style.width = "0px";
+    document.getElementById("body").style.paddingLeft = "0px";
   }
