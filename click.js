718d59f7ed628d6cd1759e7436843be15dbe30b4
RN0529 <richardngowork@gmail.com>
2021-06-29 16:37:46 -0400

    added some design changes

--- click.js
+++ click.js
@@ -23,8 +23,8 @@ function upbtnonclick(){
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("navbar").style.width = "250px";
-    document.getElementById("body").style.paddingLeft = "250px";
+    document.getElementById("navbar").style.width = "15%";
+    document.getElementById("body").style.paddingLeft = "15%";
 
   }
   
